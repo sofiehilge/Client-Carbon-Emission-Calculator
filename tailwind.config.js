f731ef87349cbeda9585b594e777c1ecedd6156e
hari0082 <2430680@rts-365.dk>
2023-12-01 15:17:36 +0100

    Add new bg to action button + fix radio buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: { Inter: ["sans-serif"], Montserrat: ["sans-serif"] },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        gradient: "linear-gradient(92deg, #2181CD 6.38%, #5EA1E2 93.85%)",
+      },
+    },
   },
   plugins: [],
 };
